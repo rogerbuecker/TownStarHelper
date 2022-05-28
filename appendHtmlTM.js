ecbb2d74d09de78f2068dfde8e060592dfd14d14
Roger Bücker <roger.buecker@googlemail.com>
2022-05-29 00:45:55 +0200

    append html via tampermonkey

---
+++ appendHtmlTM.js
@@ -0,0 +1,76 @@
+// ==UserScript==
+// @name            AppendTest
+// @description     Test
+// @version         0.1
+// @match           https://townstar.sandbox-games.com/*
+// @require         http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @grant           GM_addStyle
+// ==/UserScript==
+
+$("body").append(`
+    <div id="toggleConfigButton" style="position: fixed; bottom: 5px; right: 15px;">
+        <img src="https://user-images.githubusercontent.com/2233130/151796996-ae008a4b-ad0b-4109-96f1-f94983da2ea2.png" />
+    </div>
+`);
+
+$("body").append(`
+<!-- Tabs navs -->
+<ul class="nav nav-tabs mb-3" id="ex1" role="tablist">
+  <li class="nav-item" role="presentation">
+    <a
+      class="nav-link active"
+      id="ex1-tab-1"
+      data-mdb-toggle="tab"
+      href="#ex1-tabs-1"
+      role="tab"
+      aria-controls="ex1-tabs-1"
+      aria-selected="true"
+      >Tab 1</a
+    >
+  </li>
+  <li class="nav-item" role="presentation">
+    <a
+      class="nav-link"
+      id="ex1-tab-2"
+      data-mdb-toggle="tab"
+      href="#ex1-tabs-2"
+      role="tab"
+      aria-controls="ex1-tabs-2"
+      aria-selected="false"
+      >Tab 2</a
+    >
+  </li>
+  <li class="nav-item" role="presentation">
+    <a
+      class="nav-link"
+      id="ex1-tab-3"
+      data-mdb-toggle="tab"
+      href="#ex1-tabs-3"
+      role="tab"
+      aria-controls="ex1-tabs-3"
+      aria-selected="false"
+      >Tab 3</a
+    >
+  </li>
+</ul>
+<!-- Tabs navs -->
+
+<!-- Tabs content -->
+<div class="tab-content" id="ex1-content">
+  <div
+    class="tab-pane fade show active"
+    id="ex1-tabs-1"
+    role="tabpanel"
+    aria-labelledby="ex1-tab-1"
+  >
+    Tab 1 content
+  </div>
+  <div class="tab-pane fade" id="ex1-tabs-2" role="tabpanel" aria-labelledby="ex1-tab-2">
+    Tab 2 content
+  </div>
+  <div class="tab-pane fade" id="ex1-tabs-3" role="tabpanel" aria-labelledby="ex1-tab-3">
+    Tab 3 content
+  </div>
+</div>
+<!-- Tabs content -->
+`);
